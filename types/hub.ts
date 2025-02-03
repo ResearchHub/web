1f2146518308114cf7df0d8ee81bc8b167e598c7
nicktytarenko <nicktytarenko@gmail.com>
2025-02-03 09:51:19 +0200

    clean up

--- types/hub.ts
+++ types/hub.ts
@@ -1,14 +1,5 @@
-import { createTransformer } from './transformer';
-
-// TODO: Remove this file in favor of topics
 export type Hub = {
   name: string;
   slug: string;
   id: number;
 };
-
-export const transformHub = createTransformer<any, Hub>((raw) => ({
-  id: raw.id,
-  name: raw.name,
-  slug: raw.slug,
-}));
