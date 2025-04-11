354a2b4556e0855a60b5dc594f1452da2bbd5e64
Leevi <leevi.kerkela@protonmail.com>
2025-04-11 11:02:13 +0100

    Remove wallet and update label to 'My ResearchCoin'

--- components/menus/UserMenu.tsx
+++ components/menus/UserMenu.tsx
@@ -7,12 +7,9 @@ import type { User } from '@/types/user';
 import VerificationBanner from '@/components/banners/VerificationBanner';
 import { Avatar } from '@/components/ui/Avatar';
 import { BaseMenu, BaseMenuItem } from '@/components/ui/form/BaseMenu';
-import { useConnect, useDisconnect, useAccount } from 'wagmi';
-import { WalletModal } from 'components/modals/WalletModal';
 import { VerifyIdentityModal } from '@/components/modals/VerifyIdentityModal';
 import { VerifiedBadge } from '@/components/ui/VerifiedBadge';
 import { SwipeableDrawer } from '@/components/ui/SwipeableDrawer';
-import { Icon } from '@/components/ui/icons';
 import { ResearchCoinIcon } from '@/components/ui/icons/ResearchCoinIcon';
 import Link from 'next/link';
 
@@ -25,10 +22,6 @@ interface UserMenuProps {
   avatarSize?: number | 'sm' | 'md' | 'xs' | 'xxs';
 }
 
-function truncateWalletAddress(address: string): string {
-  return `${address.slice(0, 4)}...${address.slice(-4)}`;
-}
-
 export default function UserMenu({
   user,
   onViewProfile,
@@ -38,11 +31,6 @@ export default function UserMenu({
   avatarSize = 30,
 }: UserMenuProps) {
   const [showVerificationBanner, setShowVerificationBanner] = useState(true);
-  const [walletOptionsOpen, setWalletOptionsOpen] = useState(false);
-  useConnect();
-  const { disconnect } = useDisconnect();
-  const { address, isConnected } = useAccount();
-  const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
   const [isVerifyModalOpen, setIsVerifyModalOpen] = useState(false);
   const [internalMenuOpen, setInternalMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -75,14 +63,6 @@ export default function UserMenu({
 
   const handleLearnMore = () => {
     setIsVerifyModalOpen(true);
-  };
-
-  const handleOpenWalletModal = () => {
-    setIsWalletModalOpen(true);
-  };
-
-  const handleCloseWalletModal = () => {
-    setIsWalletModalOpen(false);
   };
 
   const handleVerifyAccount = () => {
@@ -155,7 +135,7 @@ export default function UserMenu({
           <div className="px-6 py-2 hover:bg-gray-50">
             <div className="flex items-center">
               <ResearchCoinIcon outlined className="h-5 w-5 mr-3 text-gray-500" color="#676767" />
-              <span className="text-base text-gray-700">My Wallet</span>
+              <span className="text-base text-gray-700">My ResearchCoin</span>
             </div>
           </div>
         </Link>
@@ -176,67 +156,6 @@ export default function UserMenu({
             </div>
           </div>
         )}
-
-        {/* Wallet Menu Items */}
-        {process.env.NODE_ENV !== 'production' &&
-          (isConnected ? (
-            <>
-              <div
-                className="px-6 py-2 hover:bg-gray-50"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setWalletOptionsOpen((prev) => !prev);
-                }}
-              >
-                <div className="flex items-center">
-                  <Wallet className="h-5 w-5 mr-3 text-gray-500" />
-                  <span className="text-base text-gray-700">
-                    {truncateWalletAddress(address as string)}
-                  </span>
-                </div>
-              </div>
-              {walletOptionsOpen && (
-                <>
-                  <div
-                    className="px-6 py-2 hover:bg-gray-50"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      if (address) navigator.clipboard.writeText(address);
-                    }}
-                  >
-                    <div className="flex items-center">
-                      <span className="ml-8 text-sm text-gray-500">Copy Address</span>
-                    </div>
-                  </div>
-                  <div
-                    className="px-6 py-2 hover:bg-gray-50"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      disconnect();
-                      setWalletOptionsOpen(false);
-                    }}
-                  >
-                    <div className="flex items-center">
-                      <span className="ml-8 text-sm text-gray-500">Disconnect Wallet</span>
-                    </div>
-                  </div>
-                </>
-              )}
-            </>
-          ) : (
-            <div
-              className="px-6 py-2 hover:bg-gray-50"
-              onClick={() => {
-                handleOpenWalletModal();
-                setMenuOpenState(false);
-              }}
-            >
-              <div className="flex items-center">
-                <Wallet className="h-5 w-5 mr-3 text-gray-500" />
-                <span className="text-base text-gray-700">Connect Wallet</span>
-              </div>
-            </div>
-          ))}
 
         <div className="px-6 py-2 hover:bg-gray-50" onClick={() => signOut({ callbackUrl: '/' })}>
           <div className="flex items-center">
@@ -321,7 +240,7 @@ export default function UserMenu({
                     className="h-4 w-4 mr-3 text-gray-500"
                     color="#676767"
                   />
-                  <span className="text-sm text-gray-700">My Wallet</span>
+                  <span className="text-sm text-gray-700">My ResearchCoin</span>
                 </div>
               </div>
             </Link>
@@ -336,66 +255,6 @@ export default function UserMenu({
                 </div>
               </BaseMenuItem>
             )}
-
-            {/* Wallet Menu Items */}
-            {process.env.NODE_ENV !== 'production' &&
-              (isConnected ? (
-                <>
-                  <BaseMenuItem
-                    onSelect={(e) => e.preventDefault()}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setWalletOptionsOpen((prev) => !prev);
-                    }}
-                    className="w-full px-4 py-2"
-                  >
-                    <div className="flex items-center">
-                      <Wallet className="h-4 w-4 mr-3 text-gray-500" />
-                      <span className="text-sm text-gray-700">
-                        {truncateWalletAddress(address as string)}
-                      </span>
-                    </div>
-                  </BaseMenuItem>
-                  {walletOptionsOpen && (
-                    <>
-                      <BaseMenuItem
-                        onSelect={(e) => e.preventDefault()}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          if (address) navigator.clipboard.writeText(address);
-                        }}
-                        className="w-full px-4 py-2"
-                      >
-                        <div className="flex items-center">
-                          <span className="ml-8 text-xs text-gray-500">Copy Address</span>
-                        </div>
-                      </BaseMenuItem>
-                      <BaseMenuItem
-                        onSelect={(e) => e.preventDefault()}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          disconnect();
-                          setWalletOptionsOpen(false);
-                        }}
-                        className="w-full px-4 py-2"
-                      >
-                        <div className="flex items-center">
-                          <span className="ml-8 text-xs text-gray-500">
-                            Disconnect External Wallet
-                          </span>
-                        </div>
-                      </BaseMenuItem>
-                    </>
-                  )}
-                </>
-              ) : (
-                <BaseMenuItem onClick={handleOpenWalletModal} className="w-full px-4 py-2">
-                  <div className="flex items-center">
-                    <Wallet className="h-4 w-4 mr-3 text-gray-500" />
-                    <span className="text-sm text-gray-700">Connect External Wallet</span>
-                  </div>
-                </BaseMenuItem>
-              ))}
 
             <BaseMenuItem
               onClick={() => signOut({ callbackUrl: '/' })}
@@ -422,7 +281,6 @@ export default function UserMenu({
       )}
 
       {/* Modals */}
-      <WalletModal isOpen={isWalletModalOpen} onClose={handleCloseWalletModal} />
       <VerifyIdentityModal isOpen={isVerifyModalOpen} onClose={handleCloseVerifyModal} />
     </>
   );
