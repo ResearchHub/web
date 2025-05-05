358d51ae8021ab76881d98cbd578128b7cc4c912
Tyler Diorio <109099227+TylerDiorio@users.noreply.github.com>
2025-05-05 14:19:23 -0600

    Fix editor image paths to use old app

--- components/Journal/lib/journalConstants.ts
+++ components/Journal/lib/journalConstants.ts
@@ -18,7 +18,7 @@ export const editors: Editor[] = [
     name: 'Maulik M. Dhandha, MD FAAD',
     role: 'Editor in Chief',
     bio: 'Dr. Maulik Dhandha completed his medical school and residency training in Dermatology at Saint Louis University. He practices general dermatology and is actively involved in clinical research. His work is focused on autoimmune and inflammatory skin conditions including Pemphigus Vulgaris, Psoriasis, Hidradenitis Suppurative, Atopic Dermatitis and others. His work has been published in highly reputable journals like - Journal of American Academy of Dermatology, American Journal of Dermatopathology, Autoimmunity, and British Journal of Dermatology.',
-    image: '/images/editors/MaulikDhandha.jpeg',
+    image: 'https://www.researchhub.com/static/editorial-board/MaulikDhandha.jpeg',
     authorId: '931964',
     socialLinks: {
       email: 'maulik.editor@researchhub.foundation',
@@ -30,7 +30,7 @@ export const editors: Editor[] = [
     name: 'Emilio Merheb, PhD',
     role: 'Associate Editor',
     bio: 'Dr. Emilio Merheb is an Instructor at the Diabetes, Obesity, and Metabolism Institute (DOMI) at the Icahn School of Medicine at Mount Sinai. His scientific and research expertise specializes in endocrinology, metabolism, neuroscience, biochemistry, oncology, and molecular biology. Dr. Merheb completed his doctoral training and post-doctoral fellowship at the Albert Einstein College of Medicine. His research on neurodegeneration, islet biology, and β-cell therapies appears in top journals like Cell Metabolism, PNAS, and eLife. Notably, His dedication to science resulted in the recipient of multiple awards, including the Julius Marmur Award.',
-    image: '/images/editors/EmilioMerheb.jpeg',
+    image: 'https://www.researchhub.com/static/editorial-board/EmilioMerheb.jpeg',
     authorId: '1872316',
     socialLinks: {
       email: 'emilio.editor@researchhub.foundation',
@@ -42,7 +42,7 @@ export const editors: Editor[] = [
     name: 'Attila Karsi, PhD',
     role: 'Associate Editor',
     bio: 'Dr. Attila Karsi earned his MSc and PhD degrees from Auburn University and is a tenured professor in the College of Veterinary Medicine at Mississippi State University. His research focuses on bacterial pathogenesis, host-pathogen interactions, and vaccine development. He has published over 100 peer-reviewed articles in esteemed journals, including Nucleic Acids Research, Journal of Bacteriology, Frontiers in Microbiology, and PLOS One.',
-    image: '/images/editors/AttilaKarsi.jpeg',
+    image: 'https://www.researchhub.com/static/editorial-board/AttilaKarsi.jpeg',
     authorId: '984218',
     socialLinks: {
       email: 'attila.editor@researchhub.foundation',
@@ -54,7 +54,7 @@ export const editors: Editor[] = [
     name: 'Interested in joining as an Editor?',
     role: 'Associate Editor',
     bio: "If you're interested in joining the Editorial Board, please apply here by emailing maulik.editor@researchhub.foundation. Relevant qualifications include a PhD, a strong publication record, and a passion for driving scientific progress through innovative peer review systems.",
-    image: '/images/editors/EinsteinAvatar.png',
+    image: 'https://www.researchhub.com/static/editorial-board/EinsteinAvatar.png',
     authorId: null, // No author ID for this placeholder
     socialLinks: {
       email: 'maulik.editor@researchhub.foundation',
