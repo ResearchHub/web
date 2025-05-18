0cb6848cf6fb5868b00c0eedff6f95a3318638b5
Nick Tytarenko <nick.tytarenko@sothebys.com>
2025-05-18 12:06:22 +0300

    hashclassnames

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+// CSS Module class name hashing function
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${exportName}`
+      ),
+      'md4',
+      'base64',
+      6
+    )
+    .replace(/[^a-zA-Z0-9-_]/g, '_')
+    .replace(/^(-?\d|--)/, '_$1');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -83,7 +102,7 @@ const nextConfig = {
     optimizeCss: true,
     scrollRestoration: true,
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config, { dev, isServer }) => {
     // Preserve existing alias configuration
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -106,6 +125,24 @@ const nextConfig = {
         generator: {
           filename: 'static/[hash][ext][query]',
         },
+      });
+    }
+
+    // Add CSS Module class name hashing in production
+    if (!dev) {
+      const rules = config.module.rules
+        .find((rule) => typeof rule.oneOf === 'object')
+        .oneOf.filter((rule) => Array.isArray(rule.use));
+
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes('css-loader') &&
+            !moduleLoader.loader?.includes('postcss-loader')
+          ) {
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+          }
+        });
       });
     }
 
