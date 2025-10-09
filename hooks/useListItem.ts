6c5c8d3d29001594a5ab718ba8f6eda987a71e87
Andrew Stet <stetandrew@gmail.com>
2025-10-09 08:50:59 -0700

    services and hooks for user lists

---
+++ hooks/useListItem.ts
@@ -0,0 +1,81 @@
+'use client';
+
+import toast from 'react-hot-toast';
+import { AsyncState } from '@/lib/utils';
+import { useCallback, useState } from 'react';
+import {
+  CreateListItemParams,
+  DeleteListItemParams,
+  ListItem,
+  ListItemService,
+} from '@/services/listItem.service';
+
+// ================= CREATE LIST ITEM =================
+
+type CreateListItemFn = (params: CreateListItemParams) => Promise<ListItem>;
+
+export function useCreateListItem(): AsyncState & { createListItem: CreateListItemFn } {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<AsyncState['error']>(null);
+
+  const createListItem: CreateListItemFn = useCallback(async (params) => {
+    const toastId = toast.loading('Adding to list...');
+
+    try {
+      setIsLoading(true);
+      setError(null);
+
+      const response = await ListItemService.createListItem(params);
+
+      toast.success('Added!', { id: toastId });
+
+      return response;
+    } catch (e) {
+      const msg = 'Failed to add to list';
+
+      setError(e instanceof Error ? e : new Error(msg));
+
+      toast.error(msg, { id: toastId });
+
+      throw e;
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return { isLoading, error, createListItem };
+}
+
+// ================= DELETE LIST ITEM =================
+
+type DeleteListItemFn = (params: DeleteListItemParams) => Promise<void>;
+
+export function useDeleteListItem(): AsyncState & { deleteListItem: DeleteListItemFn } {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<AsyncState['error']>(null);
+
+  const deleteListItem: DeleteListItemFn = useCallback(async (params) => {
+    const toastId = toast.loading('Removing from list...');
+
+    try {
+      setIsLoading(true);
+      setError(null);
+
+      await ListItemService.deleteListItem(params);
+
+      toast.success('Removed!', { id: toastId });
+    } catch (e) {
+      const msg = 'Failed to remove from list';
+
+      setError(e instanceof Error ? e : new Error(msg));
+
+      toast.error(msg, { id: toastId });
+
+      throw e;
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return { isLoading, error, deleteListItem };
+}
---
+++ services/list.service.ts
@@ -0,0 +1,97 @@
+import { ID } from '@/types/root';
+import { ListItem } from '@/services/listItem.service';
+import { ApiClient } from '@/services/client';
+
+export interface List {
+  id: ID;
+  name: string;
+  items?: ListItem[];
+  created_date: string;
+  updated_date: string;
+}
+
+export type CreateListParams = {
+  name: string;
+};
+
+export type GetListParams = {
+  id?: ID;
+};
+
+export type DeleteListParams = {
+  id: ID;
+};
+
+export type UpdateListParams = {
+  id: ID;
+  name?: string;
+};
+
+export class ListService {
+  private static readonly basePath = '/api/list';
+
+  // =================== API ===================
+
+  // GET /api/list/ — get lists
+  static async getLists(): Promise<List[]> {
+    try {
+      return await ApiClient.get<List[]>(this.basePath);
+    } catch (e) {
+      throw new Error(`Failed to get lists: ${this.getErrorMsg(e)}`);
+    }
+  }
+
+  // GET /api/list/{id}/ — get a list
+  static async getList(params: GetListParams): Promise<List> {
+    try {
+      return await ApiClient.get<List>(this.getIdPath(params.id));
+    } catch (e) {
+      throw new Error(`Failed to get list: ${this.getErrorMsg(e)}`);
+    }
+  }
+
+  // POST /api/list/ — create a list
+  static async createList(params: CreateListParams): Promise<List> {
+    try {
+      return await ApiClient.post<List>(this.basePath, params);
+    } catch (e) {
+      throw new Error(`Failed to create list: ${this.getErrorMsg(e)}`);
+    }
+  }
+
+  // DELETE /api/list/{id}/ — delete a list
+  static async deleteList(params: DeleteListParams): Promise<void> {
+    try {
+      await ApiClient.delete(this.getIdPath(params.id));
+    } catch (e) {
+      throw new Error(`Failed to delete list: ${this.getErrorMsg(e)}`);
+    }
+  }
+
+  // PATCH /api/list/{id}/ — update a list
+  static async updateList(params: UpdateListParams): Promise<List> {
+    try {
+      return await ApiClient.patch<List>(this.getIdPath(params.id), params);
+    } catch (e) {
+      throw new Error(`Failed to update list: ${this.getErrorMsg(e)}`);
+    }
+  }
+
+  // ================= HELPERS =================
+
+  static getErrorMsg(error: any): string {
+    if (error instanceof Error) {
+      return error.message;
+    }
+
+    if (typeof error === 'string') {
+      return error;
+    }
+
+    return 'An unknown error occurred';
+  }
+
+  static getIdPath(id: ID): string {
+    return `${this.basePath}/${id}/`;
+  }
+}
