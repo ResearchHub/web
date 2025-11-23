24067a201569ff26bd2f51dd19c8c6433c4784be
Michael Canova <michael_canova-at-174250149065>
2025-11-23 02:03:37 -0500

    [User Lists] Small Linter Fix

--- app/layouts/TopBar.tsx
+++ app/layouts/TopBar.tsx
@@ -22,10 +22,12 @@ import { Tooltip } from '@/components/ui/Tooltip';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse as faHouseSolid } from '@fortawesome/pro-solid-svg-icons';
-import { faHouse as faHouseLight } from '@fortawesome/pro-light-svg-icons';
+import {
+  faHouse as faHouseLight,
+  faBookmark as faBookmarkLight,
+} from '@fortawesome/pro-light-svg-icons';
 import { faCommentsQuestion } from '@fortawesome/pro-light-svg-icons';
 import { faGrid3 as faGrid3Light } from '@fortawesome/pro-light-svg-icons';
-import { faBookmark as faBookmarkLight } from '@fortawesome/pro-light-svg-icons';
 import { calculateProfileCompletion } from '@/utils/profileCompletion';
 import { colors } from '@/app/styles/colors';
 import { getTopicEmoji } from '@/components/Topic/TopicEmojis';
