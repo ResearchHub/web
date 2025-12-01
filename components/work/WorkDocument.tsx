e837eb3fef8f59872e6fa332999e355c0e017ec0
Kobe Attias <kobeattias@gmail.com>
2025-12-01 16:05:56 -0500

    [PDF] Extend pdf width beyond 100%

--- components/work/WorkDocument.tsx
+++ components/work/WorkDocument.tsx
@@ -127,7 +127,7 @@ export const WorkDocument = ({ work, metadata, defaultTab = 'paper' }: WorkDocum
               return (
                 pdfFormat?.url &&
                 !pdfUnavailable && (
-                  <div className="bg-white rounded-lg shadow-sm border mb-6 relative">
+                  <div className="bg-white rounded-lg shadow-sm border mb-6 relative overflow-x-auto">
                     {work.pdfCopyrightAllowsDisplay ? (
                       <DocumentViewer
                         url={pdfFormat.internalUrl || pdfFormat.url}
