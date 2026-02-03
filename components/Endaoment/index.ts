359c0db8610e47aa90c43ca031ab667bd42717f7
Gregor Zurowski <gregor@zurowski.org>
2026-02-03 12:13:28 +0100

    Add public exports for Endaoment module

---
+++ components/Endaoment/index.ts
@@ -0,0 +1,5 @@
+export { EndaomentConnectButton } from './EndaomentConnectButton';
+export { useConnectEndaoment } from './lib/hooks/useConnectEndaoment';
+export { useEndaomentCallback } from './lib/hooks/useEndaomentCallback';
+export { useEndaomentStatus } from './lib/hooks/useEndaomentStatus';
+export { connectEndaomentAccount, getEndaomentStatus } from '@/services/endaoment.service';
