566582c8ba430467e0fa83b64f04b8e2a6743f73
Kobe Attias <kobeattias@gmail.com>
2026-05-05 07:43:00 -0400

    wideContent option for pageLayout to avoid sidebar

--- app/fund/dashboard/page.tsx
+++ app/fund/dashboard/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = buildOpenGraphMetadata({
 
 export default function FunderDashboardRoute() {
   return (
-    <PageLayout rightSidebar={false}>
+    <PageLayout rightSidebar={false} wideContent>
       <FunderDashboardPage />
     </PageLayout>
   );
