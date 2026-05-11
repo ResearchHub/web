0b6928f5c8a3c487905b7152cfb53f8b420c598e
Michael Canova <michael_canova-at-174250149065>
2026-05-11 10:55:48 -0400

    [SEO] Better organizing of components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,13 @@
-import { SubtabsNav, type SubtabsSection } from './SubtabsNav';
-import { Hero } from './Hero';
-import { FundedAt } from './FundedAt';
-import { Problem } from './Problem';
-import { Solution } from './Solution';
-import { FundingMarketplace } from './FundingMarketplace';
-import { ResearchCoin } from './ResearchCoin';
-import { PartnershipSection } from './PartnershipSection';
-import { TeamSection } from './TeamSection';
-import { CTASection } from './CTASection';
+import { SubtabsNav, type SubtabsSection } from './components/SubtabsNav';
+import { Hero } from './components/Hero';
+import { FundedAt } from './components/FundedAt';
+import { Problem } from './components/Problem';
+import { Solution } from './components/Solution';
+import { FundingMarketplace } from './components/FundingMarketplace';
+import { ResearchCoin } from './components/ResearchCoin';
+import { PartnershipSection } from './components/PartnershipSection';
+import { TeamSection } from './components/TeamSection';
+import { CTASection } from './components/CTASection';
 import { LandingPageFooter } from '@/components/landing/LandingPageFooter';
 
 const sections: ReadonlyArray<SubtabsSection> = [
