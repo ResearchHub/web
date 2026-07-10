a39cf396724e54ee90d7f22b0f914c21a943be8d
nicktytarenko <nicktytarenko@gmail.com>
2026-07-10 16:35:13 +0300

    Refactor LeftSidebarContainer to remove loading skeleton and directly import LeftSidebar component

--- app/layouts/components/LeftSidebarContainer.tsx
+++ app/layouts/components/LeftSidebarContainer.tsx
@@ -1,48 +1,7 @@
 'use client';
 
-import { Suspense } from 'react';
-import dynamic from 'next/dynamic';
 import { cn } from '@/lib/utils';
-
-const LeftSidebar = dynamic(() => import('../LeftSidebar').then((mod) => mod.LeftSidebar), {
-  ssr: true,
-  loading: () => <LeftSidebarSkeleton />,
-});
-
-function LeftSidebarSkeleton() {
-  return (
-    <div className="h-full flex flex-col bg-white animate-pulse">
-      <div className="p-4 pt-[10px]">
-        <div className="h-[38px] w-[38px] ml-1 bg-gray-200 rounded" />
-      </div>
-
-      <div className="px-4 mt-6">
-        <div className="h-11 w-full bg-gray-200 rounded-lg" />
-      </div>
-
-      <div className="flex-1 mt-2 overflow-hidden px-4 py-4 space-y-1.5">
-        {Array.from({ length: 5 }, (_, i) => (
-          <div key={i} className="flex items-center w-full px-5 py-3.5 rounded-lg">
-            <div className="h-[26px] w-[26px] bg-gray-200 rounded flex-shrink-0 mr-4" />
-            <div className="h-4 bg-gray-200 rounded" style={{ width: `${56 + (i % 3) * 16}px` }} />
-          </div>
-        ))}
-      </div>
-
-      <div className="px-4 py-6 border-t border-gray-200">
-        <div className="flex items-center space-x-4 mb-4">
-          {Array.from({ length: 4 }, (_, i) => (
-            <div key={i} className="h-5 w-5 bg-gray-200 rounded" />
-          ))}
-        </div>
-        <div className="space-y-2">
-          <div className="h-3 w-24 bg-gray-200 rounded" />
-          <div className="h-3 w-32 bg-gray-200 rounded" />
-        </div>
-      </div>
-    </div>
-  );
-}
+import { LeftSidebar } from '../LeftSidebar';
 
 interface LeftSidebarContainerProps {
   isOpen: boolean;
@@ -71,9 +30,7 @@ export function LeftSidebarContainer({ isOpen, isCompact }: LeftSidebarContainer
         isOpen ? '!translate-x-0' : '!-translate-x-full'
       )}
     >
-      <Suspense fallback={<LeftSidebarSkeleton />}>
-        <LeftSidebar />
-      </Suspense>
+      <LeftSidebar />
     </div>
   );
 }
