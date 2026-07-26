eac1fb5f98f06270088f0a866362a8e20f2270c5
michaelcanova <mcanova.gp@gmail.com>
2026-07-26 18:39:35 -0400

    [Journal] Cover image bugfix

--- types/note.ts
+++ types/note.ts
@@ -179,7 +179,7 @@ export const transformNote = createTransformer<any, Note>((raw) => {
     documentType,
     proposalId,
     registeredReportPrefill: transformRegisteredReportPrefill(raw.registered_report_prefill),
-    image: raw.registered_report_prefill?.preview_img ?? null,
+    image: raw.registered_report_prefill?.image_url ?? null,
     topics: transformTopicsFromSources(
       raw.registered_report_prefill?.topics,
       raw.registered_report_prefill?.hubs,
