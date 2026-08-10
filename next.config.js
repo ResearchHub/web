f8dde2e98ad021b05da9c16bbe34be9113ceec71
Gregor Zurowski <gregor@zurowski.org>
2026-08-10 12:55:47 +0200

    Add image qualities allowlist

--- next.config.js
+++ next.config.js
@@ -179,6 +179,7 @@ const nextConfig = {
       },
     ],
     formats: ['image/webp', 'image/avif'],
+    qualities: [75, 95],
   },
   productionBrowserSourceMaps: process.env.VERCEL_ENV === 'preview',
   modularizeImports: {
